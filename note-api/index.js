526cee882233f778d4853573f4778e713aceb374
Caio Vieira <caio@institutototum.com.br>
2023-03-23 22:13:14 -0300

    adicionou o cors e readme ao projeto

--- note-api/index.js
+++ note-api/index.js
@@ -3,13 +3,14 @@ const express = require('express')
 const noteRoute = require('./routes/note')
 const authRoute = require('./routes/auth')
 const userRoute = require('./routes/user')
+const cors = require('cors')
 const authMiddleware = require('./middlewares/authMiddleware')
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 
 app.use('/api', userRoute, authRoute)
-
 app.use('/api', authMiddleware, noteRoute)
 
 app.listen(process.env.PORT, () => {
